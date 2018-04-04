a35332791922a7fa65106782424752e7092c8c96
shaiful <shaiful.software@daffodilvarsity.edu.bd>
2018-04-05 02:12:50 +0600

    fdg

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,23 +4,12 @@ const Web3= require('web3');
 
 const web3= new  Web3(ganache.provider()) ;
 
-beforeEach(()=>{
-    var coinbase = web3.eth.coinbase;
-    //console.log(coinbase);
-
-    //var balance = web3.eth.getBalance(coinbase).then(fetchedAccounts=>{
-        web3.eth.getBalance(coinbase).then(fetchedAccounts=>{
+beforeEach(  ()=>{
+    // get a list of all accounts
+    web3.eth.getAccounts().then(fetchedAccounts=>{
         console.log(fetchedAccounts);
     });
-
-
-    // get a list of all accounts
-    /*web3.eth.getAccounts().then(fetchedAccounts=>{
-        console.log(fetchedAccounts);
-
-    });*/
 })
-
 
 describe('inbox test', ()=>{
     it('it deploy a contract', ()=>{ })
@@ -28,7 +17,6 @@ describe('inbox test', ()=>{
 
 
 /*
-
 class Car{
     park(){
         return 'stopped';
